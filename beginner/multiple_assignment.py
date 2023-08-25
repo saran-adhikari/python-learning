cfbe7baef8de611be2e398349c829f42fe13e8d6
Saran Adhikari <saranadhikari02@gmail.com>
2023-08-25 13:53:40 +0545

    updated folder

---
+++ beginner/multiple_assignment.py
@@ -0,0 +1,17 @@
+# name="Saran"
+# age= 18
+# attractive= True
+
+# name, age, attractive = "Saran", 18, True
+# print(name,age,attractive)
+
+name= "saran"
+# print(len(name))
+# print(name.find("a"))
+# print(name.capitalize())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("a"))
+# print(name.replace("a","A"))
+# print(name*3)
---
+++ beginner/typecast.py
@@ -0,0 +1,7 @@
+x= 1
+y = 2.0
+z= "3"
+
+print("X is: " ,float(x))
+print("Y is: ",int(y))
+print("Z is : ",int(z)*3)
