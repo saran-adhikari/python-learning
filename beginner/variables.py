6f335adc245f6ac2af327fcb48aab41d43f31f8b
Saran Adhikari <saranadhikari02@gmail.com>
2023-08-25 13:41:16 +0545

    init

---
+++ beginner/variables.py
@@ -0,0 +1,21 @@
+#variables
+
+# first_name = "Saran"
+# last_name = "Adhikari"
+# print(type(first_name))
+# print(first_name + " " + last_name)
+
+# age = 18
+# age += 1
+# print("Your age is: ",age)
+# print(type(age))
+
+# pi=3.18
+# print(pi)
+# print(type(pi))
+# print("The value of pi is: ",pi)
+
+# human= True
+# print(human)
+# print(type(human))
+# print("Are you a human? :",human)
